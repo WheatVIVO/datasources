5d003bdf76930184d8a07bc03866e87acfa0ca16
Brian Lowe <brian@ontocale.com>
2016-12-13 17:02:17 +0200

    add argument for output file to ingest launcher class

--- datasources/src/main/java/org/wheatinitiative/vivo/datasource/LaunchIngest.java
+++ datasources/src/main/java/org/wheatinitiative/vivo/datasource/LaunchIngest.java
@@ -1,5 +1,8 @@
 package org.wheatinitiative.vivo.datasource;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,15 +17,17 @@ import com.hp.hpl.jena.rdf.model.Model;
 public class LaunchIngest {
 
     public static void main(String[] args) {
-        if(args.length < 2) {
+        if(args.length < 3) {
             System.out.println("Usage: LaunchIngest " 
-                    + "rcuk|prodinra|usda|wheatinitiative " 
+                    + "rcuk|prodinra|usda|wheatinitiative outputfile" 
                     + "queryTerm ... [queryTermN]");
         } else {
             List<String> queryTerms = new LinkedList<String>(
                     Arrays.asList(args));
             queryTerms.remove(0);
+            queryTerms.remove(0);
             String connectorName = args[0];
+            String outputFileName = args[1];
             DataSource connector = null;
             if("rcuk".equals(connectorName)) {
                 connector = new Rcuk();
@@ -41,7 +46,15 @@ public class LaunchIngest {
             connector.run();
             Model result = connector.getResult();
             if(result != null) {
-                result.write(System.out, "N3");    
+                //result.write(System.out, "N3");
+                File outputFile = new File(outputFileName);
+                FileOutputStream fos;
+                try {
+                    fos = new FileOutputStream(outputFile);
+                    result.write(fos, "N3");
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
     }
