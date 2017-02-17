f29e29fd5b6dcaa0585d9406fc9e23ad1c258906
Brian Lowe <brian@ontocale.com>
2017-02-17 17:21:02 +0200

    handling of resumptionTokens in Prodinra

--- datasources/src/main/java/org/wheatinitiative/vivo/datasource/connector/prodinra/Prodinra.java
+++ datasources/src/main/java/org/wheatinitiative/vivo/datasource/connector/prodinra/Prodinra.java
@@ -1,14 +1,17 @@
 package org.wheatinitiative.vivo.datasource.connector.prodinra;
 
-import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.client.utils.URIBuilder;
 import org.wheatinitiative.vivo.datasource.DataSource;
 import org.wheatinitiative.vivo.datasource.connector.DataSourceBase;
 import org.wheatinitiative.vivo.datasource.util.http.HttpUtils;
@@ -21,7 +24,13 @@ import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.NodeIterator;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 public class Prodinra extends DataSourceBase implements DataSource {
 
@@ -34,6 +43,13 @@ public class Prodinra extends DataSourceBase implements DataSource {
     private static final String NAMESPACE_ETC = PRODINRA_ABOX_NS + "n";
     private static final String SPARQL_RESOURCE_DIR = "/prodinra/sparql/";
     
+    private static final Property RESUMPTION_TOKEN = ResourceFactory.createProperty(
+            "http://www.openarchives.org/OAI/2.0//resumptionToken");
+    private static final Property TOTAL_RECORDS = ResourceFactory.createProperty(
+            "http://ingest.mannlib.cornell.edu/generalizedXMLtoRDF/0.1/completeListSize");
+    private static final Property VITRO_VALUE = ResourceFactory.createProperty(
+            "http://vitro.mannlib.cornell.edu/ns/vitro/0.7#value");
+    
     private Model result;
     
     private HttpUtils httpUtils = new HttpUtils();
@@ -43,14 +59,24 @@ public class Prodinra extends DataSourceBase implements DataSource {
     @Override
     public void runIngest() {
         try { 
-            String records = listRecords();
-            Model model = transformToRDF(records);
-            log.info(model.size() + " statements before filtering");
-            model = filter(model);
-            log.info(model.size() + " statements after filtering");
-            result = model;
-        } catch (IOException e) {
-            log.error(e, e);
+            OaiModelIterator it = new OaiModelIterator(
+                    this.getConfiguration().getServiceURI(), 
+                    METADATA_PREFIX);
+            log.info(it.totalRecords() + " total records");
+            Model result = ModelFactory.createDefaultModel();
+            int page = 0;
+            while(it.hasNext()) {
+                page++;
+                Model model = transformToRDF(it.next());
+                log.info(model.size() + " statements before filtering");
+                model = filter(model);
+                log.info(model.size() + " statements after filtering");
+                result.add(model);
+                log.info(result.size() + " statements after page " + page);
+            }            
+            this.result = result;
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
     }
     
@@ -106,8 +132,7 @@ public class Prodinra extends DataSourceBase implements DataSource {
         return relevantResources;
     }
     
-    protected Model transformToRDF(String records) {
-        Model m = xmlToRdf.toRDF(records);
+    protected Model transformToRDF(Model m) {
         m = rdfUtils.renameBNodes(m, NAMESPACE_ETC, m);
         m = constructForVIVO(m);
         m = rdfUtils.smushResources(m, m.getProperty(
@@ -135,10 +160,105 @@ public class Prodinra extends DataSourceBase implements DataSource {
         return m;
     }
     
-    public String listRecords() throws IOException {
-        String url = ENDPOINT + "?verb=ListRecords&metadataPrefix=" 
-                + METADATA_PREFIX;
-        return httpUtils.getHttpResponse(url);
+    private class OaiModelIterator implements Iterator<Model> {
+        
+        private URI repositoryURI;
+        private String metadataPrefix;
+        
+        private Model firstResult = null;
+        private Integer totalRecords = null;
+        private String resumptionToken = null;
+        private boolean done = false;
+        
+        public OaiModelIterator(String repositoryURL, String metadataPrefix) 
+                throws URISyntaxException {
+            this.repositoryURI = new URI(repositoryURL);
+            this.metadataPrefix = metadataPrefix;
+        }
+
+        public boolean hasNext() {
+            return (firstResult != null || !done);
+        }
+
+        public Model next() {
+            if(firstResult != null) {
+                Model m = firstResult;
+                firstResult = null;
+                return m;
+            } else {
+                if(done) {
+                    throw new RuntimeException("No more items");
+                } else {
+                    return fetchNext();
+                }
+            }
+        }
+        
+        private Model fetchNext() {
+            URIBuilder uriB = new URIBuilder(repositoryURI);
+            uriB.addParameter("verb", "ListRecords");
+            if(resumptionToken != null) {
+                uriB.addParameter("resumptionToken", resumptionToken);
+            } else {
+                uriB.addParameter("metadataPrefix", metadataPrefix);
+            }
+            try {
+                String response = httpUtils.getHttpResponse(uriB.build().toString());
+                Model m = xmlToRdf.toRDF(response);
+                processResumptionToken(m);
+                if(resumptionToken == null) {
+                    done = true;
+                }
+                if(firstResult == null) {
+                    firstResult = m;
+                }
+                return m;
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }       
+        }
+
+        private void processResumptionToken(Model m) {
+            NodeIterator nit = m.listObjectsOfProperty(RESUMPTION_TOKEN);
+            while(nit.hasNext()) {
+                RDFNode n = nit.next();
+                if(n.isResource()) {
+                    if(totalRecords == null) {
+                        StmtIterator sit = n.asResource().listProperties(
+                                TOTAL_RECORDS);
+                        while(sit.hasNext()) {
+                            Statement stmt = sit.next();
+                            if(stmt.getObject().isLiteral()) {
+                                int total = stmt.getObject().asLiteral().getInt();
+                                this.totalRecords = new Integer(total);
+                            }
+                        }
+                    } 
+                    StmtIterator sit = n.asResource().listProperties(VITRO_VALUE);
+                    try {
+                        String token = null;
+                        while(sit.hasNext()) {
+                            Statement stmt = sit.next();
+                            if(stmt.getObject().isLiteral()) {
+                                token = stmt.getObject()
+                                        .asLiteral().getLexicalForm();
+                            }
+                        }
+                        this.resumptionToken = token;
+                    } finally {
+                        sit.close();
+                    }
+                }
+            }
+        }
+        
+        public int totalRecords() {
+            if(totalRecords == null) {
+                fetchNext();
+            }
+            return totalRecords;
+        }
+        
     }
 
     public Model getResult() {
