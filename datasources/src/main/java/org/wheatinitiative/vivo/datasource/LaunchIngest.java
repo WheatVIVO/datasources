ae41d42d9e8957b84cf876088ff4155ea9a84ad6
LSmyrnaios <lsmyrnaios@gmail.com>
2017-03-17 17:34:17 +0100

    Added Cordis connector

--- datasources/src/main/java/org/wheatinitiative/vivo/datasource/LaunchIngest.java
+++ datasources/src/main/java/org/wheatinitiative/vivo/datasource/LaunchIngest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.wheatinitiative.vivo.datasource.connector.cordis.Cordis;
 import org.wheatinitiative.vivo.datasource.connector.prodinra.Prodinra;
 import org.wheatinitiative.vivo.datasource.connector.rcuk.Rcuk;
 import org.wheatinitiative.vivo.datasource.connector.usda.Usda;
@@ -23,7 +24,7 @@ public class LaunchIngest {
     public static void main(String[] args) {
         if(args.length < 3) {
             System.out.println("Usage: LaunchIngest " 
-                    + "rcuk|prodinra|usda|wheatinitiative outputfile " 
+                    + "cordis|rcuk|prodinra|usda|wheatinitiative outputfile " 
                     + "queryTerm ... [queryTermN] [limit]");
             return;
         } 
@@ -58,7 +59,11 @@ public class LaunchIngest {
     
     private static DataSource getConnector(String connectorName) {
         DataSource connector = null;
-        if("rcuk".equals(connectorName)) {
+        if ("cordis".equals(connectorName)) {
+        	connector = new Cordis();
+        	connector.getConfiguration().setServiceURI(
+        			"http://cordis.europa.eu/search/result_en");
+        } else if("rcuk".equals(connectorName)) {
             connector = new Rcuk();
             connector.getConfiguration().setServiceURI(
                     "http://http://gtr.rcuk.ac.uk/gtr/api/");
