890bcbc4d8fd8080ca634c8c5a57a34bc360bc29
LSmyrnaios <lsmyrnaios@gmail.com>
2017-08-11 22:49:44 +0200

    Improve code readability.

--- datasources/src/main/java/org/wheatinitiative/vivo/datasource/connector/tamu/Tamu.java
+++ datasources/src/main/java/org/wheatinitiative/vivo/datasource/connector/tamu/Tamu.java
@@ -5,17 +5,23 @@ import org.wheatinitiative.vivo.datasource.connector.VivoDataSource;
 
 import com.hp.hpl.jena.rdf.model.Model;
 
+
+
 public class Tamu extends VivoDataSource implements DataSource {
 
     private static final String TAMU_VIVO_URL = "http://scholars.library.tamu.edu/vivo";
    
+    
     @Override 
     protected String getRemoteVivoURL() {
         return TAMU_VIVO_URL;
     }
     
+    
     @Override
     protected Model mapToVIVO(Model model) {
+    	// No additional mapping is required.
+    	// VIVO Tamu has the latest ontology version.
     	return model;
     }
     
