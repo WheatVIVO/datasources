32efce3d7c23e74230e3166d8c36989c18f74cab
Brian Lowe <brian@ontocale.com>
2018-03-07 17:03:14 +0200

    Connect USDA organizations to USA entity

--- datasources/src/main/java/org/wheatinitiative/vivo/datasource/connector/usda/Usda.java
+++ datasources/src/main/java/org/wheatinitiative/vivo/datasource/connector/usda/Usda.java
@@ -4,6 +4,8 @@ import org.wheatinitiative.vivo.datasource.DataSource;
 import org.wheatinitiative.vivo.datasource.VivoVocabulary;
 import org.wheatinitiative.vivo.datasource.connector.VivoDataSource;
 
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
@@ -24,7 +26,26 @@ public class Usda extends VivoDataSource implements DataSource {
     protected Model mapToVIVO(Model model) {        
         // USDA VIVO is still using ontology version 1.5
         Model updated = updateToOneSix(model);
+        updated = locateOrgsInUsa(updated);
         return updateRoleRealization(updated);
+    }
+    
+    protected Model locateOrgsInUsa(Model input) {
+        String construct = "CONSTRUCT { \n"  
+                + "<http://aims.fao.org/aos/geopolitical.owl#United_States_of_America> "
+                + "    <http://purl.obolibrary.org/obo/RO_0001015> ?x \n" 
+                + "} WHERE { \n" 
+                + "  ?x a <" + VivoVocabulary.ORGANIZATION.getURI() + "> \n" 
+                + "} \n";
+        QueryExecution qe = QueryExecutionFactory.create(construct, input);
+        try {
+            Model result = qe.execConstruct();
+            return input.add(result);
+        } finally {
+            if(qe != null) {
+                qe.close();
+            }
+        }
     }
     
     // USDA's VIVO seems to use vivo:roleContributesTo in an unexpected way
