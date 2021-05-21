690d3b1729da60f4e5a5fae012e02f1fd8c8d2d7
Brian Lowe <brian@ontocale.com>
2021-05-21 17:52:54 +0300

    Use OpenAIRE publication search API in place of OAI/PMH

--- datasources/src/main/java/org/wheatinitiative/vivo/datasource/connector/openaire/OpenAire.java
+++ datasources/src/main/java/org/wheatinitiative/vivo/datasource/connector/openaire/OpenAire.java
@@ -1,7 +1,6 @@
 package org.wheatinitiative.vivo.datasource.connector.openaire;
 
-// Java imports
-import java.net.URI;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,13 +8,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-// Apache imports
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.http.client.utils.URIBuilder;
-
-// Wheatinitiative imports
 import org.wheatinitiative.vivo.datasource.DataSource;
 import org.wheatinitiative.vivo.datasource.connector.ConnectorDataSource;
 import org.wheatinitiative.vivo.datasource.util.IteratorWithSize;
@@ -23,44 +21,29 @@ import org.wheatinitiative.vivo.datasource.util.http.HttpUtils;
 import org.wheatinitiative.vivo.datasource.util.xml.XmlToRdf;
 import org.wheatinitiative.vivo.datasource.util.xml.rdf.RdfUtils;
 
-// Jena imports
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.NodeIterator;
-import com.hp.hpl.jena.rdf.model.Property;
-import com.hp.hpl.jena.rdf.model.RDFNode;
-import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.ResourceFactory;
-import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-
-
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
 
 public class OpenAire extends ConnectorDataSource implements DataSource {
 	
 	public static final Log log = LogFactory.getLog(OpenAire.class);
 	
 	private static final String OPEN_AIRE_API_URL = "http://api.openaire.eu/";
-	private static final String OPEN_AIRE_OAI_PMH = OPEN_AIRE_API_URL + "oai_pmh/";
-	private static final String OPEN_AIRE_TBOX_NS = OPEN_AIRE_OAI_PMH;
-	private static final String OPEN_AIRE_ABOX_NS = OPEN_AIRE_TBOX_NS + "individual/";
+	private static final String SEARCH_PUBLICATIONS = OPEN_AIRE_API_URL + "search/publications";
+	private static final String SEARCH_PROJECTS = OPEN_AIRE_API_URL + "search/publications";
+	private static final String OPEN_AIRE_ABOX_NS = "http://vivo.openaire.eu/individual/";
 	
 	private static final String NAMESPACE_ETC = OPEN_AIRE_ABOX_NS + "n";
 	
-	private static final String METADATA_PREFIX = "oaf";
+	private static final int RESULTS_PER_PAGE = 50;
 	
 	private static final String SPARQL_RESOURCE_DIR = "/openaire/sparql/";
-	
-	private static final Property RESUMPTION_TOKEN = ResourceFactory
-			.createProperty("http://www.openarchives.org/OAI/2.0//resumptionToken");
-	private static final Property TOTAL_RECORDS = ResourceFactory
-			.createProperty("http://ingest.mannlib.cornell.edu/generalizedXMLtoRDF/0.1/completeListSize");
-	private static final Property VITRO_VALUE = ResourceFactory
-			.createProperty("http://vitro.mannlib.cornell.edu/ns/vitro/0.7#value");
 	
 	private HttpUtils httpUtils = new HttpUtils();
 	private XmlToRdf xmlToRdf = new XmlToRdf();
@@ -69,131 +52,90 @@ public class OpenAire extends ConnectorDataSource implements DataSource {
 	
 	@Override
 	protected IteratorWithSize<Model> getSourceModelIterator() {
-		try {
-			return new OaiModelIterator(this.getConfiguration().getServiceURI(), METADATA_PREFIX);
-		} catch (URISyntaxException e) {
-			throw new RuntimeException(e);
-		}
+	    try {
+	        return new OpenAireIterator();
+	    } catch (IOException e) {
+            log.error(e, e);
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            log.error(e, e);
+            throw new RuntimeException(e);
+        }
 	}
 	
 	
-	private class OaiModelIterator implements IteratorWithSize<Model> {
+	private class OpenAireIterator implements IteratorWithSize<Model> {		
+
+	    private List<Integer> totals = new ArrayList<Integer>();
+		private int currentQueryTerm = 0;
+		private int currentPage = 1;
 		
-		private URI repositoryURI;
-		
-		private String metadataPrefix;
-		
-		private Model cachedResult = null;
-		private Integer totalRecords = null;
-		private String projectsResumptionToken = null;
-		private String pubsResumptionToken = null;
-		private boolean projectsDone = false;
-		private boolean pubsDone = false;
-		
-		
-		/**
-		 * Iterate through OpenAIRE's OAI protocol.
-		 */
-		public OaiModelIterator(String repositoryURL, String metadataPrefix) throws URISyntaxException {
-			this.repositoryURI = new URI(repositoryURL);
-			this.metadataPrefix = metadataPrefix;
-		}
-		
+		public OpenAireIterator() throws IOException, URISyntaxException {
+		    int queryTermsSize = getConfiguration().getQueryTerms().size();
+		    log.info(queryTermsSize + " query terms");
+		    Pattern r = Pattern.compile("<total>(\\d+)</total>");
+			for(String keyword : getConfiguration().getQueryTerms()) {
+			    URIBuilder b = new URIBuilder(SEARCH_PUBLICATIONS);
+			    b.addParameter("size", Integer.toString(RESULTS_PER_PAGE));
+			    b.addParameter("keywords", keyword);
+			    b.addParameter("format", "xml");
+			    String url = b.toString();
+			    log.info("Retrieving " + url);
+			    String xml = httpUtils.getHttpResponse(url);
+			    //log.info(xml);
+			    Matcher m = r.matcher(xml);
+			    if(m.find()) {
+			        String recordsStr = m.group(1);
+			        log.info("Found " + recordsStr + " records for " + keyword);
+			        int records = Integer.parseInt(recordsStr); 
+			        int total = records / RESULTS_PER_PAGE;
+			        if(records % RESULTS_PER_PAGE > 0) {
+			            total++;
+			        }
+			        totals.add(total);
+			    }
+			}
+		}		
 		
 		/**
 		 * Check if there are still records to retrieve.
 		 */
 		public boolean hasNext() {
-			return (cachedResult != null || !(projectsDone && pubsDone));
-		}
-		
+			return (currentQueryTerm < getConfiguration().getQueryTerms().size() 
+			        && currentPage <= totals.get(currentQueryTerm));
+		}		
 		
 		/**
 		 * Iterate over different chunks of data.
 		 */
 		public Model next() {
-			
-			Model model = ModelFactory.createDefaultModel();
-			
-			if (cachedResult != null) {
-				model = cachedResult;
-				cachedResult = null;
-				
-			} else {
-				// Do multiple fetches depending on the different kind of data.
-				
-				if (pubsDone) {
-					log.info("No more publications.");
-				} else {
-					model.add( fetchNextPublication(!CACHE) );
-				}
-				
-				if (projectsDone) {
-					log.info("No more projects.");
-				} else {
-					model.add( fetchNextProject(!CACHE) );
-				}
-				
-				if ( projectsDone && pubsDone ) {
-					throw new RuntimeException("No more items!");
-				}
+			Model nextModel = fetchNextPublication();
+			currentPage++;
+			if(currentPage >= totals.get(currentQueryTerm)) {
+			    currentQueryTerm++;
+	            currentPage = 1;
 			}
-			
-			return model;
-		}
-		
-		
-		private final static boolean CACHE = true;
-		
-		
-		/**
-		 * Cache the first results in order to retrieve the records number.
-		 */
-		private void cacheNext() {
-			fetchNextPublication(CACHE);
-			fetchNextProject(CACHE);
-		}
-		
+			return nextModel;
+		}		
 		
 		/**
 		 * Fetch the publications' related data.
 		 */
-		private Model fetchNextPublication(boolean cacheResult) {
-			
-			URIBuilder uriB = new URIBuilder(repositoryURI);
-			uriB.addParameter("verb", "ListRecords");
-			if (pubsResumptionToken != null) {
-				uriB.addParameter("resumptionToken", pubsResumptionToken);
-			} else {
-				uriB.addParameter("set", "openaire");
-				// "openaire" -> resultSet for all the pubs
-				uriB.addParameter("metadataPrefix", metadataPrefix);
-			}
-			try {
+		private Model fetchNextPublication() {		
+            try {
+			    URIBuilder uriB = new URIBuilder(SEARCH_PUBLICATIONS);
+			    uriB.addParameter("page", Integer.toString(currentPage));
+			    uriB.addParameter("keywords", getConfiguration().getQueryTerms()
+			            .get(currentQueryTerm));
+			    uriB.addParameter("format", "xml");
 				String request = uriB.build().toString();
 				log.info(request);
 				String response = httpUtils.getHttpResponse(request);
-				Model model = xmlToRdf.toRDF(response);
-				processResumptionToken(model, "publication");
-				if (pubsResumptionToken == null) {
-					pubsDone = true;
-					log.info("No more pub's resumption token -- done.");
-				}
-				if (cacheResult) {
-					cachedResult = model;
-				}
-				
-				model.add( getPubRelatedProjects( model ) );
-				
-				return model;
-				
+				Model model = xmlToRdf.toRDF(response);				
+				model.add( getPubRelatedProjects( model ) );				
+				return model;				
 			} catch (Exception e) {
-				if (this.pubsResumptionToken != null) {
-					this.pubsResumptionToken = guessAtNextResumptionToken(this.pubsResumptionToken);
-				}
-				else {
-					log.error(e, e);
-				}
+			    log.error(e, e);
 				throw new RuntimeException(e);
 			}
 		}
@@ -211,7 +153,7 @@ public class OpenAire extends ConnectorDataSource implements DataSource {
 	    	
 	    	while ( listIt.hasNext() ) {
 				try {
-					URIBuilder uriB = new URIBuilder( OPEN_AIRE_API_URL + "search/projects" );
+					URIBuilder uriB = new URIBuilder(SEARCH_PROJECTS);
 					uriB.addParameter( "keywords", listIt.next().toString() );
 					uriB.addParameter( "format", "xml" );
 					
@@ -262,132 +204,33 @@ public class OpenAire extends ConnectorDataSource implements DataSource {
 	    		if ( selectExec != null )
 	    			selectExec.close();
 	    	}
-	    }
-	    
+	    }    
 	    
 	    /**
 		 * Fetch the projects' related data.
 		 */
 		private Model fetchNextProject(boolean cacheResult) {
-			
-			URIBuilder uriB = new URIBuilder(repositoryURI);
-			uriB.addParameter("verb", "ListRecords");
-			if (projectsResumptionToken != null) {
-				uriB.addParameter("resumptionToken", projectsResumptionToken);
-			} else {
-				uriB.addParameter("set", "projects");
-				uriB.addParameter("metadataPrefix", metadataPrefix);
-			}
-			try {
+	        try {
+			    URIBuilder uriB = new URIBuilder(OPEN_AIRE_API_URL);		
 				String request = uriB.build().toString();
 				log.info(request);
 				String response = httpUtils.getHttpResponse(request);
 				Model model = xmlToRdf.toRDF(response);
-				processResumptionToken(model, "project");
-				if (projectsResumptionToken == null) {
-					projectsDone = true;
-					log.info("No more project's resumption token -- done.");
-				}
-				if (cacheResult) {
-					cachedResult = model;
-				}
-				
-				return model;
-				
+				return model;				
 			} catch (Exception e) {
-				if (this.projectsResumptionToken != null) {
-					this.projectsResumptionToken = guessAtNextResumptionToken(this.projectsResumptionToken);
-				}
 				throw new RuntimeException(e);
 			}
-		}
-		
-		
-		/**
-		 * Get the resumption token, which, the server is giving us,
-		 * in order to continue to retrieve the different pieces of data.
-		 */
-		private void processResumptionToken(Model model, String dataType) {
-			/*
-			 * Because the server is making the data available in pieces..
-			 * without an indicator to testify that this exact client is still requesting data..
-			 * the server would sent the first piece over and over again.
-			 * (It would think that every time the request is made by a different client.)
-			 */
-			NodeIterator nit = model.listObjectsOfProperty(RESUMPTION_TOKEN);
-			String token = null;
-			while (nit.hasNext()) {
-				RDFNode n = nit.next();
-				if (n.isResource()) {
-					if (totalRecords == null) {
-						StmtIterator sit = n.asResource().listProperties(TOTAL_RECORDS);
-						while (sit.hasNext()) {
-							Statement stmt = sit.next();
-							if (stmt.getObject().isLiteral()) {
-								int total = stmt.getObject().asLiteral().getInt();
-								this.totalRecords = new Integer(total);
-							}
-						}
-					}
-					StmtIterator sit = n.asResource().listProperties(VITRO_VALUE);
-					try {
-						while (sit.hasNext()) {
-							Statement stmt = sit.next();
-							if (stmt.getObject().isLiteral()) {
-								token = stmt.getObject().asLiteral().getLexicalForm();
-							}
-						}
-					} catch (Exception e) {
-						log.error(e, e);
-						throw new RuntimeException(e);
-					} finally {
-						sit.close();
-					}
-				}
-			}
-			log.debug("Token: " + token);
-			
-			if ( dataType == "project" )
-				this.projectsResumptionToken = token;
-			else if ( dataType == "publication" )
-				this.pubsResumptionToken = token;
-			else {
-				log.info("The method: processResumptionToken(Model, String)"
-						+ "recieved an invalid value for its \"dataType\" parameter.");
-				// In case of an addition of a new dataType, the existing "if-else" statement should get enlarged.
-				throw new RuntimeException();
-			}
-		}
-		
-		
-		/**
-		 * Guess the resumption token when some of the data is wrong/corrupted.
-		 */
-		private String guessAtNextResumptionToken(String resumptionToken) {
-			/*
-			 * In this case, we will just lose this specific piece of data,
-			 * but we will still be able to retrieve everything else.
-			 */
-			try {
-				String[] tokens = resumptionToken.split("!");
-				int cursor = Integer.parseInt(tokens[1], 10);
-				cursor = cursor + 200;
-				return tokens[0] + "!" + cursor + "!" + tokens[2] + "!" + tokens[3] + "!" + tokens[4];
-			} catch (Exception e) {
-				log.error(e, e);
-				return null;
-			}
-		}
-		
+		}		
 		
         /**
          * The size of the model.
          */
 		public Integer size() {
-			if (totalRecords == null) {
-				cacheNext();
+			int size = 0;
+			for(int total : totals) {
+			    size += total;
 			}
-			return totalRecords;
+			return size;
 		}
 		
 	}
@@ -579,7 +422,6 @@ public class OpenAire extends ConnectorDataSource implements DataSource {
         
         return subgraph;
     }
-
 
     @Override
     protected String getPrefixName() {
